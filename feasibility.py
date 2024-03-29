9049a81700648db6d25df8101d5746b770ebabea
Emmanuel Junior Wafo Wembe <juniorkelvin04@gmail.com>
2024-03-29 15:38:04 +0000

    Added description of functions in feasibility

--- feasibility.py
+++ feasibility.py
@@ -1,16 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
 from .solving_equation import eulerexplicite,RK4
 from .Lower_closedset import pos_traj
+
+# Checking if a trajectory is feasible
+
 def is_feasible_euler_case1(x_0,u, d, T, N_euler, f, g, proj = lambda x: x):
-    Solu,Timestep = eulerexplicite(t_0 = 0, t_f=T, f=f, u=u, d=d , x_0 = x_0, n =N_euler, proj = proj)
-    k = [g(i) == 0 for i in Solu]
-    if any(k):
+    """Check if a trajectory initialised at x_0 under the control input and disturbance is feasible
+        up to a timetamp T, via computing the entire trajectory 
+        using an euler explicite scheme to solve the initial value problem
+    Args:
+        x_0 (float or array(float)): initial value
+        u (function): Control input
+        d (function): disturbance input
+        T (float): Final Time 
+        N_euler (int): number of the euler scheme
+        f (function): dynamics of the system 
+        g (function): indicator of the state set 
+        proj (function, optional): Acorrection function for numerical problems. Defaults to identity.
+
+    Returns:
+        Bool: Feasibility of the trajectory
+    """
+    Solu,_ = eulerexplicite(t_0 = 0, t_f=T, f=f, u=u, d=d , x_0 = x_0, n =N_euler, proj = proj)
+    k = [g(i) == 0 for i in Solu] 
+    if any(k): #Solution must not leave the set. 
         return False
     for i in range(2, N_euler):
-        if pos_traj(Solu[i], Solu[:i]): 
+        if pos_traj(Solu[i], Solu[:i]): # The first time is sufficient since 
             return True 
     return False
 def is_feasible_rk4_case1(x_0,u, d, T, N_euler, f, g, proj = lambda x: x):
-    Solu,Timestep = RK4(t_0 = 0, t_f=T, f=f, u=u, d=d , x_0 = x_0, n =N_euler,proj = proj)
+    """Check if a trajectory initialised at x_0 under the control input and disturbance is feasible
+        up to a timetamp T,  using a RK4 scheme to solve the initial value problemm
+    Args:
+        x_0 (float or array(float)): initial value
+        u (function): Control input
+        d (function): disturbance input
+        T (float): Final Time 
+        N_euler (int): number of the euler scheme
+        f (function): dynamics of the system 
+        g (function): indicator of the state set 
+        proj (function, optional): Acorrection function for numerical problems. Defaults to identity.
+
+    Returns:
+        Bool: Feasibility of the trajectory
+    """
+    Solu,_ = RK4(t_0 = 0, t_f=T, f=f, u=u, d=d , x_0 = x_0, n =N_euler,proj = proj)
     k = [g(i) == 0 for i in Solu]
     if any(k):
         return False
@@ -20,7 +56,22 @@ def is_feasible_rk4_case1(x_0,u, d, T, N_euler, f, g, proj = lambda x: x):
     return False
 
 def is_feasible_rk4_case2(x_0,u, d, T, N_euler, f, g, t_0 = 0,proj = lambda x: x):
-    #Solu,Timestep = RK4(t_0 = 0, t_f=T, f=f, u=u, d=d , x_0 = x_0, n =N_euler,proj = proj)
+    """Check if a trajectory initialised at x_0 under the control input and disturbance is feasible
+        up to a timetamp T,  using a RK4 scheme to solve the initial value problem, by generating the trajectory
+        if we find an answer before T
+    Args:
+        x_0 (float or array(float)): initial value
+        u (function): Control input
+        d (function): disturbance input
+        T (float): Final Time 
+        N_euler (int): number of the euler scheme
+        f (function): dynamics of the system 
+        g (function): indicator of the state set 
+        proj (function, optional): Acorrection function for numerical problems. Defaults to identity.
+
+    Returns:
+        Bool: Feasibility of the trajectory
+    """
     h = (T- t_0)/N_euler
     Timestep = [t_0 + i*h for i in range(N_euler+1)]
     Timestep[N_euler] = T
@@ -44,3 +95,6 @@ def is_feasible_rk4_case2(x_0,u, d, T, N_euler, f, g, t_0 = 0,proj = lambda x: x
             return Reach , Timestep[:i+1], 1
         Reach.append(proj(new))
     return Reach, Timestep, 2
+
+if __name__ =="__main__":
+    print(True)
